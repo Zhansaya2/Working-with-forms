506acf8cd5adf0df72072fc4ab5120a8d28b2a8f
Zhansaya2 <zhansaya.dauletbaeva1229@gmail.com>
2020-10-24 04:13:42 +0500

    first commit

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -80,7 +80,16 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'library',
+#        'USER': 'library_admin',
+#        'PASSWORD': 'qwerty123',
+#        'HOST': 'localhost',
+#        'PORT': '5432'
+#    }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
